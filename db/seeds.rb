a49f4fc5177610ff47fd564c9aa971dc4d45be10
mcclains2003 <mcclains2003@gmail.com>
2018-04-19 14:05:12 -0500

    Created a seed for populating teams.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Uncomment out for Heroku seeding
+
+# teams = ["Arizona Cardinals", 
+#     "Atlanta Falcons", 
+#     "Baltimore Ravens", 
+#     "Buffalo Bills", 
+#     "Carolina Panthers", 
+#     "Chicago Bears",
+#     "Cincinnati Bengals", 
+#     "Cleveland Browns",
+#     "Dallas Cowboys",
+#     "Denver Broncos",
+#     "Detroit Lions",
+#     "Green Bay Packers",
+#     "Houston Texans",
+#     "Indianapolis Colts",
+#     "Jacksonville Jaguars",
+#     "Kansas City Chiefs",
+#     "Los Angeles Rams",
+#     "Los Angeles Chargers",
+#     "Miami Dolphins",
+#     "Minnesota Vikings",
+#     "New England Patriots",
+#     "New Orleans Saints", 
+#     "New York Giants", 
+#     "New York Jets", 
+#     "Oakland Raiders", 
+#     "Philadelphia Eagles", 
+#     "Pittsburgh Steelers", 
+#     "San Francisco 49ers", 
+#     "Seattle Seahawks", 
+#     "Tampa Bay Buccaneers", 
+#     "Tennessee Titans", 
+#     "Washington Redskins",]
+
+#     teams.each do |team|
+#         Team.create!(name: team)
+#     end
