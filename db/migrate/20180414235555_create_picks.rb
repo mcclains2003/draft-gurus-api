181d1e03517e912a77ce7158ed48ef41c327d235
mcclains2003 <mcclains2003@gmail.com>
2018-04-20 10:19:03 -0500

    Made sure picks belonged to a team also.

--- db/migrate/20180414235555_create_picks.rb
+++ db/migrate/20180414235555_create_picks.rb
@@ -1,9 +1,10 @@
 class CreatePicks < ActiveRecord::Migration[5.2]
   def change
     create_table :picks do |t|
-      t.string :number
+      t.integer :number
       t.belongs_to :round, index: true
       t.belongs_to :player, index: true
+      t.belongs_to :team, index: true
 
       t.timestamps
     end
