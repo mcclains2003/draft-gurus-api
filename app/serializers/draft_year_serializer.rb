3639a42b6a41f68c8706599743a6572df986d74a
mcclains2003 <mcclains2003@gmail.com>
2018-05-09 09:30:05 -0500

    Created custom draft year serializer with nested attributes using AMS.

--- app/serializers/draft_year_serializer.rb
+++ app/serializers/draft_year_serializer.rb
@@ -1,5 +1,21 @@
 class DraftYearSerializer < ActiveModel::Serializer
-  attributes :id, :year
+  attributes :id, 
+             :year,
 
-  has_many :drafts
+             # has_many
+             :drafts
+
+  def drafts
+    customized_drafts = []
+
+    object.drafts.each do |draft|
+      custom_draft = draft.attributes
+
+      custom_draft[:picks] = draft.picks.collect{|pick| pick.slice(:id, :number, :round)}
+
+      customized_drafts.push(custom_draft)
+    end
+
+    return customized_drafts
+  end
 end
